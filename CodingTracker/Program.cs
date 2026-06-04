b262a5a65dd2c8180ab55ed9ab1a24f33a863c4f
Ajay5351 <morlaajay9676@gmail.com>
2026-06-04 12:00:59 +0530

    Intial Commit

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,3 +1,14 @@
-﻿Console.WriteLine("Hello, C# Academy! one more modification"); Console.WriteLine("Hello, C# Academy! one more modification"); Console.WriteLine("Hello, C# Academy! one more modification");
-Console.WriteLine("Hello, C# Academy! one more modification"); Console.WriteLine("Hello, C# Academy! one more modification");
-Console.WriteLine("Hello, C# Academy! one more modification");
+﻿void MyFirstMethod()
+{
+   // Do Something
+}
+
+void MySecondMethod()
+{
+    // Do Something
+}
+
+void MyThirdMethod()
+{
+    // Do Something
+}
