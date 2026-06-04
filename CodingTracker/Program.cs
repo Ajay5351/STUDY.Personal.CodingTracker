4eeaf04857f6f38397a78eb1bd1ec0e315ca23e7
AJAY M <morlaajay9676@gmail.com>
2026-06-04 11:46:58 +0530

    Keeping One Statement

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,3 +1,1 @@
-﻿Console.WriteLine("Hello, C# Academy! one more modification"); Console.WriteLine("Hello, C# Academy! one more modification"); Console.WriteLine("Hello, C# Academy! one more modification");
-Console.WriteLine("Hello, C# Academy! one more modification"); Console.WriteLine("Hello, C# Academy! one more modification");
-Console.WriteLine("Hello, C# Academy! one more modification");
+﻿Console.WriteLine("Hello, C# Academy! one more modification");
